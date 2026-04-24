2cc0ece51785961e8082e5ec5c656b464d9b7df6
JenoraTech <jenoratech@gmail.com>
2026-04-24 11:45:40 +0100

    Update callback URL for payment initialization

--- routes/payment.js
+++ routes/payment.js
@@ -35,7 +35,7 @@ router.post("/initialize-payment", async (req, res) => {
       {
         email,
         amount: amount * 100,
-        callback_url: "https://tailorbizapp.web.app/payment-success",
+        callback_url: "https://tailorapp.jenoratech.com.ng/payment-success",
         metadata: { deviceId },
       },
       {
